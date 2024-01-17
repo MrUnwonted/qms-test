22d02c07db2d3aaecdaa42bf7a62ec0be2dab701
camerinfolks <camerinfolksremote@gmail.com>
2024-01-17 16:48:49 +0530

    Before Registration

---
+++ Backend/src/main/java/com/camerin/QMS/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.camerin.QMS.repository;
+
+import com.camerin.QMS.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Boolean existsByUsername(String username);
+}
