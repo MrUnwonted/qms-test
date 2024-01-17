e45d6ef091c1ef678bb38d842d109b0133538c3f
camerinfolks <camerinfolksremote@gmail.com>
2024-01-17 16:33:27 +0530

    First Commit

---
+++ Backend/src/test/java/com/camerin/QMS/QmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camerin.QMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
